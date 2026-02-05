14f742be7985eac7eb7dd9c9eb417ca98d4d79ee
Peter De Bruycker <peter.de.bruycker@gmail.com>
2026-02-05 12:16:59 +0100

    minor change to trigger github workflow

--- src/test/java/org/ungoverned/jafl/words/SpellerTest.java
+++ src/test/java/org/ungoverned/jafl/words/SpellerTest.java
@@ -26,6 +26,7 @@ package org.ungoverned.jafl.words;
  * #L%
  */
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Locale;
@@ -51,6 +52,6 @@ public class SpellerTest {
 
     @Test
     public void getSpellerWithUnknownLocaleReturnsDefaultLocale() {
-        assertTrue(Speller.getSpeller(new Locale("zz")).equals(Speller.getSpeller()));
+        assertEquals(Speller.getSpeller(new Locale("zz")), Speller.getSpeller());
     }
 }
